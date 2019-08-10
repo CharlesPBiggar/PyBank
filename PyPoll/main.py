ff30d12238b2c051e7d9235ff29b3067c4630e0f
CharlesPBiggar <charles.p.biggar@gmail.com>
2019-08-10 00:00:45 -0400

    updated PyPoll to include results 2-5

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,38 +15,72 @@ with open(election_data_path, newline='') as csvfile:
     #skip header
     csv_header = next(csvreader)
 
+    total_votes = []
+    Khan_votes = []
+    Correy_votes = []
+    Li_votes = []
+    OTooley_votes = []    
+    
     #read each row of data
     for row in csvreader:
         
-        #The total number of votes cast
+        total_votes.append(str(row[1]))
         
+        if row[2] == "Khan":
+            Khan_votes.append(str(row[2]))
 
-        #A complete list of candidates who received votes
+        if row[2] == "Correy":
+            Correy_votes.append(str(row[2]))
+        
+        if row[2] == "Li":
+            Li_votes.append(str(row[2]))
+        
+        if row[2] == "O'Tooley":
+            OTooley_votes.append(str(row[2]))              
+                
+    #The total number of votes cast
+    total_vote_count = len(total_votes)
+    print(total_vote_count)
+    #Calculating total votes for each candidate
+    Khan_total = len(Khan_votes)
+    print(Khan_total)
+    Correy_total = len(Correy_votes)
+    print(Correy_total)
+    Li_total = len(Li_votes)
+    print(Li_total)
+    OTooley_total = len(OTooley_votes)
+    print(OTooley_total)
 
+    #Calculating the percentage of votes each candidate won
+    pct_Khan = (Khan_total / total_vote_count) * 100
+    pct_Khan_rounded = round(pct_Khan, 2)
+    print(pct_Khan_rounded)
+    pct_Correy = (Correy_total / total_vote_count) * 100
+    pct_Correy_rounded = round(pct_Correy, 2)
+    print(pct_Correy_rounded)
+    pct_Li = (Li_total / total_vote_count) * 100
+    pct_Li_rounded = round(pct_Li, 2)
+    print(pct_Li_rounded)
+    pct_OTooley = (OTooley_total / total_vote_count) * 100
+    pct_OTooley_rounded = round(pct_OTooley)
+    print(pct_OTooley_rounded)
+    
 
-        #The percentage of votes each candidate won
-
-
-        #The total number of votes each candidate won
-
-
-        #The winner of the election based on popular vote.
-
-
+    
 #Export File
 
     #Results
-    #print("Election Results")
-    #print("-----------------------")
-    #print("Total Votes: " + str(total_votes))
-    #print("-----------------------")
-    #print()
-    #print()
-    #print()
-    #print()
-    #print("-----------------------")
-    #print("Winner: " + election_winner)
-    #print("-----------------------")
+    print("Election Results")
+    print("-----------------------")
+    print("Total Votes: " + str(total_vote_count))
+    print("-----------------------")
+    print("Khan: " + str(pct_Khan_rounded) + "% " + " (" + str(Khan_total) + ")")
+    print("Correy: " + str(pct_Correy_rounded) + "% " + " (" + str(Correy_total) + ")")
+    print("Li: " + str(pct_Li_rounded) + "% " + " (" + str(Li_total) + ")")
+    print("O'Tooley: " + str(pct_OTooley_rounded) + "% " + " (" + str(OTooley_total) + ")")
+    print("-----------------------")
+    #print("Winner: " + str(election_winner))
+    print("-----------------------")
 
 
 
@@ -61,4 +95,3 @@ with open(election_data_path, newline='') as csvfile:
 #O'Tooley: 3.000% (105630)
 #-------------------------
 #Winner: Khan
-#-------------------------
